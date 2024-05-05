4c0608b6e49f8d06d35ab1fa44c840c1728a28fa
Space Nerd <71553327+SpaceNerde@users.noreply.github.com>
2024-05-05 20:01:32 +0200

    welp

--- client/src/main.rs
+++ client/src/main.rs
@@ -2,12 +2,30 @@ use std::net::TcpStream;
 use std::io::stdin;
 use std::io::Write;
 use std::io::BufRead;
+use std::io::BufReader;
+use std::thread;
 
 fn main() -> std::io::Result<()> {
     let mut stream = TcpStream::connect("127.0.0.1:80")?;
 
-    // Active messaging loop
+    // message reciving thread
+    let stream_clone = stream.try_clone().unwrap();
 
+    thread::spawn(move || {
+        let stream = stream_clone;
+        
+        let mut reader = BufReader::new(&stream);
+        let mut buffer = String::new();
+
+        while reader.read_line(&mut buffer).unwrap() > 0 {
+            println!("TEST");
+            let message = buffer.trim_end().to_string();
+            println!("{:?}", message);
+            buffer.clear();
+        }
+    });
+
+    // message sending loop
     loop {
         let mut buffer = String::new();
         let mut handle = stdin();
--- server/src/main.rs
+++ server/src/main.rs
@@ -4,30 +4,60 @@ use std::io::Read;
 use std::str;
 use std::io::BufReader;
 use std::io::BufRead;
+use std::sync::mpsc;
+use std::io::Write;
+use std::sync::Arc;
+use std::sync::Mutex;
 
 fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:80")?;
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                thread::spawn(move || {
-                    println!("{:?} just connected", stream.peer_addr());
+    let mut clients: Vec<TcpStream> = vec![];
+    let shared_clients = Arc::new(Mutex::new(clients));
+    let shared_clients_thread = Arc::clone(&shared_clients);
 
-                    let mut reader = BufReader::new(&stream);
-                    let mut buffer = String::new();
+    let (tx, rx) = mpsc::channel();
+  
+    thread::spawn(move || {
+        for stream in listener.incoming() {
+            match stream {
+                Ok(mut stream) => {
+                    let stream_clone = stream.try_clone();
 
-                    while reader.read_line(&mut buffer).unwrap() > 0 {
-                        println!("{:?}", buffer.trim_end());
-                        buffer.clear();
-                    }
-                });
-            }
-            Err(e) => {
+                    let mut clients = shared_clients.lock().unwrap();
+                    clients.push(stream_clone.unwrap());
 
+                    let tx_clone = tx.clone();
+
+                    thread::spawn(move || {
+                        println!("{:?} just connected", stream.peer_addr());
+
+                        let mut reader = BufReader::new(&stream);
+                        let mut buffer = String::new();
+
+
+                        while reader.read_line(&mut buffer).unwrap() > 0 {
+                            let message = buffer.trim_end().to_string();
+                            tx_clone.send(message).unwrap();
+                            buffer.clear();
+                        }
+                    });
+                }
+                Err(e) => {
+
+                }
             }
         }
+    });
+
+
+    while let received = rx.recv() {
+        let mut clients = shared_clients_thread.lock().unwrap();
+        for mut client in clients.iter() {
+            client.write(received.clone().unwrap().as_bytes()).unwrap();
+        }
     }
+
 
     Ok(())
 }
