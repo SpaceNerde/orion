58edc9e5481cb4d24b8fcbd75be53cf2736e74f5
Space Nerd <71553327+SpaceNerde@users.noreply.github.com>
2024-05-06 19:33:51 +0200

    impl Client and start error handling

--- server/src/main.rs
+++ server/src/main.rs
@@ -8,11 +8,49 @@ use std::sync::mpsc;
 use std::io::Write;
 use std::sync::Arc;
 use std::sync::Mutex;
+use std::io::ErrorKind;
+
+// propper disconnect and error handling
+//
+// To do:
+//  - message gets lost
+//  - cant clone stuff
+//  - server shutsdown
+//  - weird message format?
+
+struct Group {
+
+}
+
+struct Client {
+    stream: TcpStream,
+    username: String,
+}
+
+impl Client {
+    fn new(stream: TcpStream, username: String) -> Self {
+        Self {
+            stream,
+            username,
+        }
+    }
+    
+    // To lazy to work on a state system so i just straight up drop the stream to disconnect :|
+    fn disconnect(&mut self) {
+        drop(self);
+    }
+
+    fn send_message(&mut self, message: String) {
+        self.stream.write(format!("{}: {}", self.username, message).as_bytes()).expect("Could not write to client");
+    }
+}
 
 fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:80")?;
-
-    let mut clients: Vec<TcpStream> = vec![];
+    
+    // Create list of all connected clients
+    // TODO: Make Fucking Groups and then make this code a litte more clean
+    let mut clients: Vec<Client> = vec![];
     let shared_clients = Arc::new(Mutex::new(clients));
     let shared_clients_thread = Arc::clone(&shared_clients);
 
@@ -23,9 +61,18 @@ fn main() -> std::io::Result<()> {
             match stream {
                 Ok(mut stream) => {
                     let stream_clone = stream.try_clone();
-
+                    
+                    // Add client to List of clients 
+                    // TODO: Change this shit into clean groups later aswell!!
                     let mut clients = shared_clients.lock().unwrap();
-                    clients.push(stream_clone.unwrap());
+
+                    // create new client
+                    let mut client = Client::new(
+                        stream_clone.unwrap(), 
+                        "TEST USER".to_string(),
+                    );
+
+                    clients.push(client);
 
                     let tx_clone = tx.clone();
 
@@ -36,10 +83,25 @@ fn main() -> std::io::Result<()> {
                         let mut buffer = String::new();
 
 
-                        while reader.read_line(&mut buffer).unwrap() > 0 {
-                            let message = buffer.to_string();
-                            tx_clone.send(message).unwrap();
-                            buffer.clear();
+                        loop {
+                            match reader.read_line(&mut buffer) {
+                                Ok(_) => {
+                                    let message = buffer.to_string();
+                                    tx_clone.send(message).unwrap();
+                                    buffer.clear();
+                                },                  
+                                Err(e) if e.kind() == ErrorKind::ConnectionAborted => {
+                                    println!("INFO: Client just disconnected.");
+                                    break;
+                                },
+                                Err(e) if e.kind() == ErrorKind::ConnectionReset => {
+                                    println!("INFO: Client reset connection.");
+                                    break;
+                                }
+                                Err(e) => {
+                                    panic!("ERROR: {:?}", e);
+                                }
+                            }
                         }
                     });
                 }
@@ -51,10 +113,10 @@ fn main() -> std::io::Result<()> {
     });
 
 
-    while let received = rx.recv() {
+    while let received = rx.recv().unwrap() {
         let mut clients = shared_clients_thread.lock().unwrap();
-        for mut client in clients.iter() {
-            client.write(received.clone().expect("could not get received message").as_bytes()).expect("Could not write to client");
+        for mut client in clients.iter_mut() {
+            client.send_message(received.clone());
         }
     }
 
