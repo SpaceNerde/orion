b1fe34020853734718e345598a077671ac473944
Space Nerd <71553327+SpaceNerde@users.noreply.github.com>
2024-05-06 20:08:21 +0200

    code refactor and error handling!

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,36 +1,69 @@
+use std::io::{stdin, BufRead, BufReader, ErrorKind, Write};
 use std::net::TcpStream;
-use std::io::stdin;
-use std::io::Write;
-use std::io::BufRead;
-use std::io::BufReader;
 use std::thread;
 
 fn main() -> std::io::Result<()> {
-    let mut stream = TcpStream::connect("127.0.0.1:80")?;
+    match TcpStream::connect("127.0.0.1:80") {
+        Ok(mut stream) => {
+            // message reciving thread
+            let stream_clone = stream.try_clone().unwrap();
 
-    // message reciving thread
-    let stream_clone = stream.try_clone().unwrap();
+            thread::spawn(move || {
+                let mut reader = BufReader::new(&stream_clone);
+                let mut buffer = String::new();
 
-    thread::spawn(move || {
-        let mut reader = BufReader::new(&stream_clone);
-        let mut buffer = String::new();
+                loop {
+                    match reader.read_line(&mut buffer) {
+                        Ok(_) => {
+                            let message = buffer.trim_end().to_string();
+                            println!("{:?}", message);
+                            buffer.clear();
+                        }
+                        Err(e) if e.kind() == ErrorKind::ConnectionAborted => {
+                            println!("INFO: Connection to server lost.");
+                            break;
+                        }
+                        Err(e) if e.kind() == ErrorKind::ConnectionReset => {
+                            println!("INFO: Connection to server lost.");
+                            break;
+                        }
+                        Err(e) => {
+                            panic!("ERROR: {:?}", e);
+                        }
+                    }
+                }
+            });
 
+            // message sending loop
+            loop {
+                let mut buffer = String::new();
+                let handle = stdin();
 
-        while reader.read_line(&mut buffer).unwrap() > 0 {
-            let message = buffer.trim_end().to_string();
-            println!("{:?}", message);
-            buffer.clear();
+                match handle.read_line(&mut buffer) {
+                    Ok(_) => match stream.write(buffer.as_bytes()) {
+                        Ok(_) => {}
+                        Err(e) if e.kind() == ErrorKind::ConnectionReset => {
+                            println!("INFO: There is no connection to the host.");
+                            break;
+                        }
+                        Err(e) => {
+                            panic!("ERROR: {:?}", e);
+                        }
+                    },
+                    Err(e) if e.kind() == ErrorKind::ConnectionReset => {
+                        println!("INFO: There is no connection to the host.");
+                        break;
+                    }
+                    Err(e) => {
+                        panic!("ERROR: {:?}", e);
+                    }
+                }
+            }
         }
-    });
-
-    // message sending loop
-    loop {
-        let mut buffer = String::new();
-        let mut handle = stdin();
-
-        handle.read_line(&mut buffer)?;
-
-        stream.write(buffer.as_bytes())?;
+        Err(_) => {
+            // You dont always have to panic if something goes wrong ;)
+            println!("ERROR: Could not connect to server!");
+        }
     }
 
     Ok(())
