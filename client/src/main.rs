b50daea2afaa96f35519955f96f740fe8edaa495
Space Nerd <71553327+SpaceNerde@users.noreply.github.com>
2024-05-10 09:32:30 +0200

    create little input field

--- client/src/main.rs
+++ client/src/main.rs
@@ -2,7 +2,7 @@ use std::io::{stdin, BufRead, BufReader, ErrorKind, Write, stdout};
 use std::net::TcpStream;
 use std::thread;
 use crossterm::{
-    event::{self, KeyCode, KeyEventKind},
+    event::{self, KeyCode, KeyEventKind, Event},
     terminal::{
         disable_raw_mode, enable_raw_mode, EnterAlternateScreen,
         LeaveAlternateScreen,
@@ -11,14 +11,67 @@ use crossterm::{
 };
 use ratatui::{
     prelude::{CrosstermBackend, Stylize, Terminal},
-    widgets::Paragraph,
+    widgets::{Paragraph, Borders, Block},
+    Frame
 };
+
+struct ClientApp {
+    input: String,
+    char_index: usize,
+}
+
+impl ClientApp {
+    fn new() -> Self {
+        Self {
+            input: String::new(),
+            char_index: 0,
+        }
+    }
+
+    fn move_cursor(&mut self, direction: i8) {
+        match direction {
+            -1 => {
+                let cursor_move = self.char_index.saturating_sub(1);
+                self.char_index = cursor_move.clamp(0, self.input.chars().count());
+            },
+            1 => {
+                let cursor_move = self.char_index.saturating_add(1);
+                self.char_index = cursor_move.clamp(0, self.input.chars().count());
+            }
+            _ => {
+                panic!("Direction can only range from -1 to 1!");
+            }
+        }
+    }
+
+    fn enter_char(&mut self, io_char: char) {
+        let index = self.byte_index();
+        self.input.insert(index, io_char);
+        self.move_cursor(1);
+    }
+
+    fn byte_index(&mut self) -> usize {
+        self.input
+            .char_indices()
+            .map(|(i, _)| i)
+            .nth(self.char_index)
+            .unwrap_or(self.input.len())
+    }
+
+    fn send_message(&mut self, stream: &mut TcpStream) {
+        stream.write(self.input.as_bytes());
+        self.input = String::new();
+        self.char_index = 0;
+    }
+}
 
 fn main() -> std::io::Result<()> {
     stdout().execute(EnterAlternateScreen)?;
     enable_raw_mode()?;
     let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
     terminal.clear()?;
+
+    let mut client_app = ClientApp::new();
 
     match TcpStream::connect("127.0.0.1:80") {
         Ok(mut stream) => {
@@ -51,36 +104,31 @@ fn main() -> std::io::Result<()> {
                 }
             });
 
-            // message sending loop
+            // Main Loop Cause
             loop {
-                if event::poll(std::time::Duration::from_millis(16))? {
-                    if let event::Event::Key(key) = event::read()? {
-                        if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
-                            break;
-                        }
-                    }
-                }
+                terminal.draw(|f| ui(f, &client_app))?;
 
-                let mut buffer = String::new();
-                let handle = stdin();
-
-                match handle.read_line(&mut buffer) {
-                    Ok(_) => match stream.write(buffer.as_bytes()) {
-                        Ok(_) => {}
-                        Err(e) if e.kind() == ErrorKind::ConnectionReset => {
-                            println!("INFO: There is no connection to the host.");
-                            break;
+                if let Event::Key(key) = event::read()? { 
+                    if key.kind == KeyEventKind::Press {
+                        match key.code {
+                            KeyCode::Enter => client_app.send_message(&mut stream),
+                            KeyCode::Char(to_insert) => {
+                                client_app.enter_char(to_insert);
+                            }
+                            KeyCode::Backspace => {
+                                // TODO
+                            }
+                            KeyCode::Left => {
+                                client_app.move_cursor(-1);
+                            }
+                            KeyCode::Right => {
+                                client_app.move_cursor(1);
+                            }
+                            KeyCode::Esc => {
+                                return Ok(());
+                            }
+                            _ => {}
                         }
-                        Err(e) => {
-                            panic!("ERROR: {:?}", e);
-                        }
-                    },
-                    Err(e) if e.kind() == ErrorKind::ConnectionReset => {
-                        println!("INFO: There is no connection to the host.");
-                        break;
-                    }
-                    Err(e) => {
-                        panic!("ERROR: {:?}", e);
                     }
                 }
             }
@@ -89,9 +137,15 @@ fn main() -> std::io::Result<()> {
             // You dont always have to panic if something goes wrong ;)
             println!("ERROR: Could not connect to server!");
         }
-    }
+    }   
 
     stdout().execute(LeaveAlternateScreen)?;
     disable_raw_mode()?;
     Ok(())
 }
+
+fn ui(f: &mut Frame, app: &ClientApp) {
+    let input = Paragraph::new(app.input.as_str()).block(Block::bordered().title("Input"));
+    f.render_widget(input, f.size());
+}
+
