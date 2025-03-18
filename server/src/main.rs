1c9c102e87cce5234dadd94264c53a4f80a015ff
SpaceNerde <spacenerddev@gmail.com>
2025-03-18 14:38:35 +0100

    Simple Chat Server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,18 +1,81 @@
-use std::net::TcpStream;
+use std::collections::HashMap;
+use std::net::{IpAddr, SocketAddr, TcpStream};
+use std::ops::Deref;
+use std::sync::mpsc::{channel, Receiver, Sender};
+use std::sync::Arc;
 use std::thread;
 use std::{net::TcpListener, result};
-use std::io::Write;
+use std::io::{Read, Write};
 
 type Result<T> = result::Result<T, ()>;
 
-fn handle_connection(mut stream: TcpStream) -> Result<()>{
-    println!("INFO: Client Connected to Server: {}", stream.peer_addr().unwrap());
+enum Message {
+    Connect(Arc<TcpStream>),
+    Disconnect(Arc<TcpStream>),
+    New{
+        sender: Arc<TcpStream>,
+        msg: Vec<u8>
+    }
+}
+
+fn handle_server(rx: Receiver<Message>) -> Result<()> {
+    let mut client: HashMap<SocketAddr, Arc<TcpStream>> = HashMap::new();
     
-    // welcome message
-    drop(stream.write_all(b"Welcome To Orion!"));
-    
+    loop {
+        let msg = rx.recv().map_err(|e| {
+            eprintln!("ERROR: Could not receive message: {e}");
+        })?;
+        
+        match msg {
+            Message::Connect(stream) => {
+                // add new connection to list of connected clients
+                client.insert(stream.peer_addr().expect("BROKEN"), stream.clone());
+
+                stream.as_ref().write_all(b"\nWelcome to Orion!\n").expect("BROKEN");
+            },
+            Message::Disconnect(stream) => {
+                client.remove(&stream.peer_addr().expect("BROKEN"));
+
+                println!("INFO: Client disconnected: {:?}", stream.peer_addr().unwrap());
+            },
+            Message::New{sender, msg} => {
+                for (addr, client) in client.iter() {
+                    if sender.as_ref().peer_addr().expect("BROKEN") == *addr {
+                        continue;
+                    }
+                    let _ = client.as_ref().write(&msg.to_vec());
+                }
+            },
+        }
+    }
+}
+
+fn handle_connection(stream: Arc<TcpStream>, tx: Sender<Message>) -> Result<()>{
+    tx.send(Message::Connect(stream.clone())).map_err(|e| {
+        eprintln!("ERROR: Client could not Connect to server: {e}");
+    })?;
+
     // hold connection to server open
-    std::thread::sleep(std::time::Duration::new(9999999999999, 0));
+    let mut buf = vec![0; 64];
+    loop {
+        let n = stream.as_ref().read(&mut buf).map_err(|e| {
+            eprintln!("ERROR: Could not read from stream: {e}");
+            let _ = tx.send(Message::Disconnect(stream.clone()));
+        })?;
+        
+        tx.send(Message::New{
+            sender: stream.clone(), 
+            msg:buf[0..n].to_vec()
+        }).map_err(|e| {
+                eprintln!("ERROR: Could not send message over channel: {e}");
+        })?;
+
+        if n == 0 {
+            break;
+        }
+    }
+
+    let _ = tx.send(Message::Disconnect(stream.clone()));
 
     Ok(())
 }
@@ -28,12 +91,21 @@ fn main() -> Result<()> {
 
     println!("INFO: Server listening on {addr}");
 
+    let (tx, rx) = channel::<Message>();
+
+    // running server handler
+    thread::spawn(|| {
+        let _ = handle_server(rx);
+    });
+
     // accept all incoming connections
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
+                let tx = tx.clone();
+                let stream = Arc::new(stream);
                 thread::spawn(|| {
-                    let _ = handle_connection(stream);
+                    let _ = handle_connection(stream, tx);
                 });
             },
             Err(e) => {
