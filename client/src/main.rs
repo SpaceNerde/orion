238a3ef5c892971e57a56643fa6ff42acdd87b54
SpaceNerde <spacenerddev@gmail.com>
2025-04-04 20:36:47 +0200

    Create input field, broken for now wait for anathema fix

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,10 +1,6 @@
-use std::{io::Read, net::TcpStream, result, str::from_utf8, sync::{mpsc::{Receiver, channel}, Arc}, thread::{self, sleep}, time::Duration};
+use std::{io::{Read, Write}, net::TcpStream, result, str::from_utf8, sync::{mpsc::{channel, Receiver}, Arc}, thread::{self, sleep}, time::Duration};
 
-use anathema::{component::{Component, MouseState}, default_widgets::Overflow, prelude::{Document, TuiBackend}, runtime::{self, Runtime}, state::{List, State, Value}};
-
-// how to fucking implement message reciving now :,)
-// Put the TcpStream into message and on tick read from the stream and put that shit into the Value
-// List ez pz
+use anathema::{component::{Component, KeyCode, MouseState}, default_widgets::Overflow, prelude::{Document, TuiBackend}, runtime::{self, Runtime}, state::{AnyState, List, State, Value}, widgets::components::events::KeyState};
 
 type Result<T> = result::Result<T, ()>;
 
@@ -46,11 +42,11 @@ struct MessagesState {
 #[derive(Debug)]
 struct Messages {
     ts: Duration,
-    stream: TcpStream,
+    stream: Arc<TcpStream>,
 }
 
 impl Messages {
-    fn new(stream: TcpStream) -> Self {
+    fn new(stream: Arc<TcpStream>) -> Self {
         Self {
             ts: Duration::new(0, 0),
             stream
@@ -76,7 +72,7 @@ impl Component for Messages {
         }
 
         let mut buffer = vec![0; 64];
-        let n = self.stream.read(&mut buffer).expect("Something went bad when reading to buffer");
+        let n = self.stream.as_ref().read(&mut buffer).expect("Something went bad when reading to buffer");
 
         state.messages.push(Value::new(from_utf8(&mut buffer[0..n]).expect("Could not turn into utf").to_string()));
 
@@ -84,23 +80,69 @@ impl Component for Messages {
     }
 }
 
+#[derive(Debug, Default, State)]
+struct InputState {
+    input_text: Value<List<char>>
+}
 
-struct Message {
-    msg: Value<String>,
+#[derive(Debug)]
+struct Input {
+    stream: Arc<TcpStream>,
+    input_buffer: Vec<u8>,
+}
+
+impl Input {
+    fn new(stream: Arc<TcpStream>) -> Self {
+        Self { 
+            stream,
+            input_buffer: vec![]
+        }
+    }
+}
+
+impl Component for Input {
+    type State = InputState;
+    type Message = ();
+
+    fn on_key(
+            &mut self,
+            key: anathema::component::KeyEvent,
+            state: &mut Self::State,
+            mut elements: anathema::widgets::Elements<'_, '_>,
+            mut context: anathema::prelude::Context<'_, Self::State>,
+        ) {
+        if !(matches!(key.state, KeyState::Press)) {
+            return;
+        }
+        
+        // Lib is broken yeah wait for fix :,)
+
+        match key.code {
+            KeyCode::Char(c) => {
+                self.input_buffer.push(c as u8);
+                state.input_text.push_back(Value::new(c));
+            },
+            // fix for if messages does not get send
+            KeyCode::Enter => {
+                drop(self.stream.as_ref().write(&self.input_buffer));
+            },
+            _ => ()
+        }
+    }
 }
 
 fn main() -> Result<()>{
     // TODO create a proper App flow and dont hardcode all that shit
     
-    let mut stream = TcpStream::connect("127.0.0.1:8080").map_err(|e| {
+    let stream = TcpStream::connect("127.0.0.1:8080").map_err(|e| {
         // dont panic but implement proper error handling!
         panic!("ERROR: Could not connect to server!")
-    }).unwrap();
+    })?;
+    let input_stream = stream.try_clone().unwrap();
 
     let doc = Document::new("@client");
     let backend = TuiBackend::builder()
         .enable_raw_mode()
-        .enable_alt_screen()
         .enable_mouse()
         .finish()
         .unwrap();
@@ -113,12 +155,19 @@ fn main() -> Result<()>{
         Client::default(),
         ()
     ).unwrap();
-
+    
     runtime.register_component(
         "messages",
         "templates/messages.aml",
-        Messages::new(stream),
+        Messages::new(Arc::new(stream)),
         MessagesState::default()
+    ).unwrap();
+
+    runtime.register_component(
+        "input", 
+        "templates/input.aml", 
+        Input::new(Arc::new(input_stream)), 
+        InputState::default()
     ).unwrap();
 
     runtime.finish().unwrap().run();
